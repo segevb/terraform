a2d98e6eb2b61feafc1559bda961e427d9f9dfc1
Segev <segev91.b@gmail.com>
2021-01-30 15:50:26 +0200

    new

---
+++ main.tf
@@ -0,0 +1,159 @@
+# terraform commands:
+
+# terraform init
+# terraform plan
+# terraform destroy
+
+# provider declaration - hashicorp/aws
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    #   version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"  # Frankfurt region
+  access_key = " Please enter access_key "
+  secret_key = " Please enter secret_key "
+}
+
+
+# 1- Create a VPC
+# 10.122.0.0 255.255.0.0 ===> 65,536 = 2^16 
+resource "aws_vpc" "devops_2020" {
+  cidr_block = "10.122.0.0/16"
+}
+
+# 2- Create an internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.devops_2020.id
+}
+
+# 3- Create Custom Route Table
+resource "aws_route_table" "devops_rt1" {
+ vpc_id = aws_vpc.devops_2020.id
+
+ route {
+     cidr_block = "0.0.0.0/0" # IPv4
+     gateway_id = aws_internet_gateway.gw.id
+ }
+
+ route {
+     ipv6_cidr_block = "::/0" #IPv6
+     gateway_id = aws_internet_gateway.gw.id
+ }
+
+ tags = {
+   "Name" = "devops_2020"
+ }
+
+}
+
+# 4- Create network Subnet
+resource "aws_subnet" "devops_subnet-01" {
+    vpc_id = aws_vpc.devops_2020.id
+    cidr_block = "10.122.122.0/24"
+    availability_zone = "eu-central-1a" 
+    
+    tags = {
+      Name = "devops_subnet-01"
+    }
+}
+
+# 5- Associate Route table with subnet
+resource "aws_route_table_association" "a" {
+ subnet_id = aws_subnet.devops_subnet-01.id
+ route_table_id = aws_route_table.devops_rt1.id
+}
+
+# 6- Create securiry group
+resource "aws_security_group" "allow_web" {
+  name = "allow_web_traffic"
+  description = "Allow inbound web traffic"
+  vpc_id = aws_vpc.devops_2020.id
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+  }
+  
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  } 
+  
+  egress  {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "All networks allowed"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  } 
+
+  tags = {
+    "Name" = "DevOps-2020"
+  }
+
+}
+
+# 7- Create new network interface
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id =  aws_subnet.devops_subnet-01.id
+  private_ip = "10.122.122.122"
+  security_groups = [ aws_security_group.allow_web.id ]
+}
+
+# 8- Create new Elastic IP
+resource "aws_eip" "web_eip" {
+    vpc = true
+    network_interface = aws_network_interface.web-server-nic.id
+    # associate_with_private_ip = "10.122.122.122"
+    depends_on = [ aws_internet_gateway.gw ]
+}
+
+# 9- Printout the server public ip
+output "server_public_ip" {
+  value = aws_eip.web_eip.public_ip
+}
+
+# 10- Create a new ubuntu instance
+resource "aws_instance" "web_server_instance" {
+    ami = "ami-0502e817a62226e03"
+    instance_type = "t2.micro"
+    availability_zone = "eu-central-1a"
+    key_name = "nader"
+    
+    network_interface {
+      device_index = 0
+      network_interface_id = aws_network_interface.web-server-nic.id
+    }
+    user_data = <<-EOF
+
+    sudo apt update 
+    sudo apt install apache2
+    
+
+    EOF
+
+  tags = {
+    "Name" = "Nader Web Server"
+  }
+}
